c274675d80bf22fd5451e2a27f8a56ad48647faa
Haresh <haresh@Hareshs-MacBook-Pro.local>
2023-06-24 08:41:26 +0530

    added S6 model to the model.py file

--- S8/model.py
+++ S8/model.py
@@ -40,6 +40,8 @@ def conv_block(in_channels, out_channels, normalization, kernel_size = 3, pool=F
     
     return nn.Sequential(*layers)
 
+# Session 8 Models
+
 # The Network Architecture
 class NetBN(nn.Module):
     def __init__(self):
@@ -148,45 +150,9 @@ class NetGN(nn.Module):
 
         x = x.view(-1, 10)
         return F.log_softmax(x, dim=-1)
-    
-train_losses = []
-test_losses = []
-train_acc = []
-test_acc = []
-misclassified_images = []
 
-def model_train(model, device, train_loader, optimizer, epoch):
-    model.train()
-    pbar = tqdm(train_loader)
 
-    train_loss = 0
-    correct = 0
-    processed = 0
-
-    for batch_idx, (data, target) in enumerate(pbar):
-        data, target = data.to(device), target.to(device)
-        optimizer.zero_grad()
-
-        # Predict
-        output = model(data)
-
-        # Calculate loss
-        loss = F.nll_loss(output, target)
-        train_loss+=loss.item()
-
-        # Backpropagation
-        loss.backward()
-        optimizer.step()
-
-        pred = output.argmax(dim=1,keepdim=True)
-        
-        correct += pred.eq(target.view_as(pred)).sum().item()
-        processed += len(data)
-
-        pbar.set_description(desc= f'Train: Loss={loss.item():0.4f} Batch_id={batch_idx} Accuracy={100*correct/processed:0.2f}')
-
-    train_acc.append(100*correct/processed)
-    train_losses.append(train_loss/len(train_loader))
+# Session 7 Models
 
 class Net1(nn.Module):
     def __init__(self):
@@ -310,6 +276,97 @@ class Net3(nn.Module):
         x = self.conv5(x)
         x = x.view(-1, 10)
         return F.log_softmax(x, dim=-1)
+
+# Session 6 Model
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(1, 8, 3, padding=1), # Convolution layer 1 - input 28 x 28 x 1 : Output 28 x 28 x 8 : RF 3 x 3
+            nn.ReLU(), 
+            nn.BatchNorm2d(8),
+            nn.Dropout(0.05),
+            nn.Conv2d(8, 16, 3), # Convolution Layer 2 - input 28 x 28 x 8 : Output 26 x 26 x 16 : RF 5 x 5
+            nn.ReLU(), 
+            nn.BatchNorm2d(16), 
+            nn.Dropout(0.05),  
+            nn.MaxPool2d(2, 2) # Max Pooling Layer - input 26 x 26 x 16 : Output 13 x 13 x 16 : RF 10 x 10
+        )
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(16, 8, 1), # Convolution Layer 3 - input 13 x 13 x 16 : Output 13 x 13 x 8 : RF 12 x 12
+            nn.ReLU(),
+            nn.BatchNorm2d(8),
+            nn.Dropout(0.05),
+            nn.Conv2d(8, 16, 3), # Convolution Layer 4 - input 13 x 13 x 8 : Output 11 x 11 x 16 : RF 14 x 14
+            nn.ReLU(),
+            nn.BatchNorm2d(16),
+            nn.Dropout(0.05),
+            nn.Conv2d(16, 32, 3), # Convolution Layer 5 - input 11 x 11 x 16 : Output 9 x 9 x 32 : RF 16 x 16
+            nn.ReLU(),
+            nn.BatchNorm2d(32),
+            nn.Dropout(0.05),
+            nn.Conv2d(32, 32, 3), # Convolution Layer 6 - input 9 x 9 x 32 : Output 7 x 7 x 32 : RF 18 x 18
+            nn.ReLU(),
+            nn.BatchNorm2d(32),
+            nn.Dropout(0.05),
+        )
+
+        self.conv3 = nn.Sequential(
+            nn.Conv2d(32, 10, 3), # Convolution Layer 7 - input 7 x 7 x 32 : Output 5 x 5 x 10 : RF 20 x 20
+            nn.ReLU(),
+            nn.BatchNorm2d(10),
+            nn.Dropout(0.05),
+            nn.AvgPool2d(5) # Average Pooling Layer - to reduce the dimensions to 1
+        )
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        x = x.view(-1, 10)
+        return F.log_softmax(x)
+
+
+
+train_losses = []
+test_losses = []
+train_acc = []
+test_acc = []
+misclassified_images = []
+
+def model_train(model, device, train_loader, optimizer, epoch):
+    model.train()
+    pbar = tqdm(train_loader)
+
+    train_loss = 0
+    correct = 0
+    processed = 0
+
+    for batch_idx, (data, target) in enumerate(pbar):
+        data, target = data.to(device), target.to(device)
+        optimizer.zero_grad()
+
+        # Predict
+        output = model(data)
+
+        # Calculate loss
+        loss = F.nll_loss(output, target)
+        train_loss+=loss.item()
+
+        # Backpropagation
+        loss.backward()
+        optimizer.step()
+
+        pred = output.argmax(dim=1,keepdim=True)
+        
+        correct += pred.eq(target.view_as(pred)).sum().item()
+        processed += len(data)
+
+        pbar.set_description(desc= f'Train: Loss={loss.item():0.4f} Batch_id={batch_idx} Accuracy={100*correct/processed:0.2f}')
+
+    train_acc.append(100*correct/processed)
+    train_losses.append(train_loss/len(train_loader))
 
 
 def model_test(model, device, test_loader):
